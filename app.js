c804306fd16a10b2494ce79eb9c4bf78e62570a2
Chuanjo <chuanjo@hotmail.com>
2022-02-16 17:38:08 +0100

    Interaccion 2 lista

--- app.js
+++ app.js
@@ -12,14 +12,20 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+  console.log()
 });
+
+app.get("/beers", (req, res) => {
+  res.render('beers');
+})
+
+app.get("/random-beer", (req, res) => {
+  res.render('random-beer');
+})
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
